f07428530dd8420cca9d8b5a2e9b83964f1a56fc
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-05-23 03:39:26 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Welcome from './Welcome.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Welcome name={"Ozzy"} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
