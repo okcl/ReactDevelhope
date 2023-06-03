f233b522e209c8c7e6e22be60b1927ea4ba45904
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-06-04 01:49:30 +0300

    ClickCounter component with increment button

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter initalValue={20} incrementBy={2} timeout={500}/>
+        <Counter />
       </header>
     </div>
   );
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,29 +1,13 @@
 import React from "react";
-import CounterDisplay from "./CounterDisplay";
+import ClickCounter from "./ClickCounter";
 
 class Counter extends React.Component {
-    state = {
-        count: this.props.initalValue ?? 0
-    }
 
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.setState((state) => {
-                return {
-                    count: state.count + (this.props.incrementBy ?? 1),
-                }
-            })
-        }, this.props.timeout ?? 1000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
 
     render() {
         return (
             <div>
-                <CounterDisplay countState={this.state.count} />
+                <ClickCounter />
             </div>
         );
     }
