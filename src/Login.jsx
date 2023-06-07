478b63a3da51e2248b74a011be1b032fd1205bc2
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-06-07 12:56:01 +0300

    reset button added

--- src/Login.jsx
+++ src/Login.jsx
@@ -20,12 +20,24 @@ class Login extends React.Component {
         })
     }
 
+    resetState = () => {
+        this.setState({
+            username: '',
+            password: '',
+            remember: false,
+            buttonEnabled: false
+        })
+    }
+
     render() {
         return <div className="login">
             <input name="username" value={this.state.username} onChange={this.handleInputChange}/>
             <input name="password" type="password" value={this.state.password} onChange={this.handleInputChange} />
             <input name="remember" type="checkbox" checked={this.state.remember} onChange={this.handleInputChange}/> 
-            <button disabled={!this.state.buttonEnabled} onClick={() => {this.props.onLogin(this.state)}}>Login</button>
+            <div>
+                <button disabled={!this.state.buttonEnabled} onClick={() => {this.props.onLogin(this.state)}}>Login</button>
+                <button onClick={this.resetState}>Reset</button>
+            </div>
         </div>
     }
 }
