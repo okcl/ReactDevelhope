d469216954e0702cd340b374690f57c9d700f165
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-06-24 00:33:28 +0300

    custom hook for fetching data created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './index.css'
-import LoginForm from './LoginForm';
+import GithubUserList from './GithubUserList';
 
 function App() {
     return (
       <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-black text-center">
-        <LoginForm />
+        <GithubUserList />
       </div>
     )
 }
---
+++ src/GithubUser.jsx
@@ -0,0 +1,14 @@
+import useGithubUser from "./useGithubUser"
+
+function GithubUser({ username }){
+    const { data, loading, error } = useGithubUser(username)
+    return(
+        <div className="text-white">
+            {loading && <h1>Loading..</h1>}
+            {error && <h1 className="text-red">An error accoured</h1>}
+            {data && <h1>Name of the user is: {data.name}</h1>}
+        </div>
+    )
+}
+
+export default GithubUser
