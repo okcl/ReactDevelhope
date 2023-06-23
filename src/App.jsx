4c0e8baa5251b4d022a839b707f9e728a42183f5
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-06-23 16:35:48 +0300

    GithubUser component that fetches name of the user

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './index.css'
-import ClickCounter from './ClickCounter';
-import Counter from './Counter';
+import GithubUser from './GithubUser';
 
 function App() {
     return (
       <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white text-center">
-        <Counter />
+        <GithubUser username={"okcl"} />
       </div>
     )
 }
