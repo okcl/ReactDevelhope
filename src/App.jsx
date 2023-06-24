ef6dec6b3e8b72f89cb25db4916a17af92bb2d92
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-06-24 16:17:46 +0300

    useContext language display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css'
-import CarDetails from './CarDetails';
+import { LanguageContext } from './LanguageContext';
+import DisplayLanguage from './DisplayLanguage';
 
 function App() {
+  const [language, setLanguage] = useState('en')
+
+  function handleChangeLanguage(event) {
+    setLanguage(event.target.value)
+  }
     return (
-      <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-black text-center">
-        <CarDetails initialData={{model:"Tesla Model S",year:"2022",color:"Red"}} />
+      <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white text-center">
+        <div className="text-black">
+          <select value={language} onChange={handleChangeLanguage}>
+            <option value="en">English</option>
+            <option value="it">Italiano</option>
+          </select>
+        </div>  
+        <LanguageContext.Provider value={language}>
+          <DisplayLanguage />
+        </LanguageContext.Provider>
       </div>
     )
 }
---
+++ src/DisplayLanguage.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+import { LanguageContext } from "./LanguageContext"
+
+function DisplayLanguage(){
+    const language = useContext(LanguageContext)
+    return(
+        <h3>Current Language: {language}</h3>
+    )
+}
+
+export default DisplayLanguage
