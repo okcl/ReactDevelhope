2cfe02afbabd90da883f4355ab378ad7f2e80325
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-06-25 21:36:10 +0300

    github user nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ShowGithubUser from './ShowGithubUser';
 import DisplayLanguage from './DisplayLanguage';
 import Counter from './Counter';
 import LoginForm from './LoginForm';
+import GithubUserList from './GithubUserList';
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
   return (
     <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white text-center">
       <div className="bg-gray-400 hover:bg-gray-600">
-        <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/language" >Language</Link> | <Link to="/login" >Login Form</Link> | <Link to="/asdasfasd" >Invalid Page</Link>
+        <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/language" >Language</Link> | <Link to="/login" >Login Form</Link> | <Link to="/asdasfasd" >Invalid Page</Link> | <Link to={"/users"}>GitHub Users</Link>
       </div>
 
       <Routes>
@@ -21,6 +22,13 @@ function App() {
         <Route path="/counter" element={<Counter />} />
         <Route path="/language" element={<DisplayLanguage />} />
         <Route path="/login" element={<LoginForm />} />
+
+        
+        <Route path="users" element={<GithubUserList />}>
+            <Route path=":username" element={<ShowGithubUser />}></Route>
+        </Route>
+
+
         <Route path='*' element={<div><p>Not Found</p>
           <Link to="/">Go Home</Link>
         </div>} />
--- src/GithubUserList.jsx
+++ src/GithubUserList.jsx
@@ -1,33 +1,36 @@
 import { useState } from "react";
-import GithubUser from "./GithubUser";
+import { Link } from "react-router-dom";
 
-//has to be a valid username!!
+function GithubUserList() {
+  const [inputText, setInputText] = useState("");
+  const [usernames, setUsernames] = useState([]);
 
-function GithubUserList(){
-    const [inputText, setInputText] = useState("");
-    const [usernames, setUsernames] = useState([]);
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
 
-    const handleInputChange =(e) => {
-        setInputText(e.target.value);
+  const addText = () => {
+    if (inputText !== "") {
+      setUsernames((prevArray) => [...prevArray, inputText]);
+      setInputText("");
     }
+  };
 
-    const addText = () => {
-        if (inputText !== "") {
-            setUsernames((prevArray) => [...prevArray, inputText]);
-            setInputText("");
-        }
-    };
-
-    return(
-        <div className="flex flex-col">
-            <input type="text" value={inputText} onChange={handleInputChange} />
-            <button onClick={addText} className="bg-gray-300">Add Username</button>
-            <ul className="text-white">
-                {usernames.map((text, index) => (
-                    <li key={index}><GithubUser username={text} /></li>
-                ))}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="flex flex-col text-black">
+      <input type="text" value={inputText} onChange={handleInputChange} />
+      <button onClick={addText} className="bg-gray-300 hover:bg-gray-400">
+        Add Username
+      </button>
+      <ul className="text-white">
+        {usernames.map((text, index) => (
+          <li key={index} className="bg-gray-400 hover:bg-gray-600">
+            <Link to={`/users/${text}`}>{text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-export default GithubUserList
+
+export default GithubUserList;
