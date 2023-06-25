1d26eb86addc473a4b85fc1dca614ae45b20460d
Oğuzhan Akçalı <oguzkhanakcali@gmail.com>
2023-06-26 00:04:36 +0300

    user avatar feature

--- src/GithubUser.jsx
+++ src/GithubUser.jsx
@@ -7,7 +7,12 @@ function GithubUser({ username }){
             <button onClick={onRefresh}>Refresh</button>
             {isLoading && <h1>Loading..</h1>}
             {error && <h1>An error accoured</h1>}
-            {data && <h1>Name of the user is: {data.name}</h1>}
+            {data && 
+                <div className="flex flex-col items-center">
+                    <img src={data.avatar_url} className="w-20 h-20 rounded-full" alt="user_avatar"/>
+                    <h1>Name of the user is: {data.name}</h1>
+                </div>
+            }
         </div>
     )
 }
